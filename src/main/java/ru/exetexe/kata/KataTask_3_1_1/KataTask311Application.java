4156abfee1d9e1f69fe51bb9c2d61c323c976675
EXETEXE <exetexe1@email.com>
2022-07-19 21:44:17 +0300

    2022.07.19: Первый вариант решения

---
+++ src/main/java/ru/exetexe/kata/KataTask_3_1_1/KataTask311Application.java
@@ -0,0 +1,13 @@
+package ru.exetexe.kata.KataTask_3_1_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataTask311Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataTask311Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/exetexe/kata/KataTask_3_1_1/KataTask311ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.exetexe.kata.KataTask_3_1_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataTask311ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
